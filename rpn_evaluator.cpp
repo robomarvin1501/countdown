9109ce782b9cd002eb4c9c1621b427ae12f2b354
8Oldr <8Oldr@protonmail.com>
2025-07-23 13:07:27 +0300

    Improved the evaluator by removing the extra functions, and increasing code reuse

--- rpn_evaluator.cpp
+++ rpn_evaluator.cpp
@@ -1,61 +1,6 @@
 #include <cctype>
-#include <cstring>
 #include <sstream>
 #include <stack>
-
-std::stack<int> STACK;
-
-extern "C" int
-multiply ()
-{
-    int r = STACK.top ();
-    STACK.pop ();
-    int l = STACK.top ();
-    STACK.pop ();
-    STACK.push (r * l);
-    return 0;
-}
-
-extern "C" int
-add ()
-{
-    int r = STACK.top ();
-    STACK.pop ();
-    int l = STACK.top ();
-    STACK.pop ();
-    STACK.push (r + l);
-    return 0;
-}
-
-extern "C" int
-subtract ()
-{
-    int r = STACK.top ();
-    STACK.pop ();
-    int l = STACK.top ();
-    STACK.pop ();
-    if (l <= r)
-        {
-            return -1;
-        }
-    STACK.push (l - r);
-    return 0;
-}
-
-extern "C" int
-divide ()
-{
-    int r = STACK.top ();
-    STACK.pop ();
-    int l = STACK.top ();
-    STACK.pop ();
-    if (l % r != 0 || r == 0)
-        {
-            return -1;
-        }
-    STACK.push (l / r);
-    return 0;
-}
 
 extern "C" int
 evaluate (char *expression)
@@ -63,6 +8,7 @@ evaluate (char *expression)
     int total = 0;
     std::stringstream ss (expression);
     std::string token;
+    std::stack<int> STACK;
 
     while (ss >> token)
         {
@@ -73,35 +19,31 @@ evaluate (char *expression)
                 }
             else
                 {
+                    int r = STACK.top ();
+                    STACK.pop ();
+                    int l = STACK.top ();
+                    STACK.pop ();
                     switch (token[0])
                         {
                         case '*':
-                            ret = multiply ();
-                            if (ret == -1)
-                                {
-                                    return -1;
-                                }
+                            STACK.push (l * r);
                             break;
                         case '/':
-                            ret = divide ();
-                            if (ret == -1)
+                            if (l % r != 0 || r == 0)
                                 {
                                     return -1;
                                 }
+                            STACK.push (l / r);
                             break;
                         case '+':
-                            ret = add ();
-                            if (ret == -1)
-                                {
-                                    return -1;
-                                }
+                            STACK.push (l + r);
                             break;
                         case '-':
-                            ret = subtract ();
-                            if (ret == -1)
+                            if (l <= r)
                                 {
                                     return -1;
                                 }
+                            STACK.push (l - r);
                             break;
                         default:
                             break;
